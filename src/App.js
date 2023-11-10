98b1ecb5912ff5fcd2c6c5c44b5cdb9d34dd8ba1
SasankDachepalli <141177246+SasankDachepalli@users.noreply.github.com>
2023-11-10 12:46:59 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import WORDCOUNTER from './Components/WordCounter'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <WORDCOUNTER />
+    </div>
+  );
+}
+
+export default App;
